d72ebc039de6afa8083105971ee7b0cf78a2466f
davidgitz <davidgitz@gmail.com>
2015-04-28 22:49:01 -0500

    Implemented test NAV message parsing.  Implemented Servo and Drive output.

--- src/icarus_rover_mc/icarus_rover_mc.ino
+++ src/icarus_rover_mc/icarus_rover_mc.ino
@@ -1,3 +1,4 @@
+#include <Servo.h> 
 //Basic Definitions
 #define TRUE 1
 #define FALSE 0
@@ -10,10 +11,14 @@
 #define B_LED_PIN 26
 #define C_LED_PIN 25
 
+#define STEER_SERVO 12
+#define DRIVE_MOTOR 11
+
+
 //Timing Stuff Definitions
 #define FAST_LOOP 1 //1000 Hz
 #define MEDIUM_LOOP 20 //50 Hz
-#define SLOW_LOOP 100 //1 Hz
+#define SLOW_LOOP 1000 //1 Hz
 
 #define PRIORITY_HIGH 0
 #define PRIORITY_MEDIUM 1
@@ -34,6 +39,10 @@ String in_message;
 int in_message_completed = 0;
 int in_message_started = 0;
 int temp = 0;
+int value1 = 0;
+int value2 = 0;
+Servo SteerServo;
+Servo DriveMotor;
 void setup()
 {
   Serial.begin(BAUD_RATE);
@@ -45,25 +54,21 @@ void setup()
   digitalWrite(B_LED_PIN, LED_OFF);
   digitalWrite(C_LED_PIN, LED_OFF);
   in_message.reserve(200);
+  SteerServo.attach(STEER_SERVO);
+  DriveMotor.attach(DRIVE_MOTOR);
+  
 }
 void loop()
 {
   
   delay(1);
-  digitalWrite(C_LED_PIN, !digitalRead(C_LED_PIN));
-  if (in_message_completed == 1)
-  {
-        Serial.println(in_message);
-        Serial2.println(in_message);
-        in_message = "";
-        in_message_completed = 0;
-        in_message_started = 0;
-   }
-  /*
+  //digitalWrite(C_LED_PIN, !digitalRead(C_LED_PIN));
+  
+  
   //digitalWrite(A_LED_PIN, !digitalRead(A_LED_PIN));
   //digitalWrite(B_LED_PIN, !digitalRead(B_LED_PIN));
   //digitalWrite(C_LED_PIN, !digitalRead(C_LED_PIN));
-  dontime++;
+  ontime++;
   if (ontime > 100000)
   {
      ontime = 1;
@@ -88,86 +93,51 @@ void loop()
   { 
     priority_level = -1;
   }
-  (if (Serial.available() > 0)
-      {
-        //Serial.flush();
-        //digitalWrite(B_LED_PIN, !digitalRead(B_LED_PIN));
-        //in_char = Serial.read();
-        //Serial.println(in_char);
-      }
-  
   switch (priority_level)
   {
     case PRIORITY_HIGH:
       //digitalWrite(A_LED_PIN, !digitalRead(A_LED_PIN));
       break;
     case PRIORITY_MEDIUM:
-      if (in_message_complete == 1)
+      if (in_message_completed == 1)
       {
-        digitalWrite(A_LED_PIN, !digitalRead(A_LED_PIN));
-        Serial2.println(in_message);
-        in_message = "";
-        in_message_complete = 0;
+          if (in_message.substring(0,4) == "$NAV")
+          {
+            Serial.print("Got NAV Message: ");
+            value1 = getValue(in_message,',',1).toInt();
+            value2 = getValue(in_message,',',2).toInt();
+            SteerServo.write(value2);
+            DriveMotor.write(value1);
+            Serial.print(value1);
+            Serial.print(" ");
+            Serial.println(value2);
+          }
+          else if(in_message.substring(0,4) == "$SEN")
+          {
+            Serial.println("Got SEN Message.");
+          }
+          Serial.println(in_message);
+          Serial2.println(in_message);
+          in_message = "";
+          in_message_completed = 0;
+          in_message_started = 0;
       }
-      //digitalWrite(B_LED_PIN, !digitalRead(B_LED_PIN));
-       
-        memset(response,'\0',sizeof response);
-        digitalWrite(B_LED_PIN,FALSE);
-        do
-        {
-          digitalWrite(B_LED_PIN,TRUE);
-          in_byte = Serial.read();
-          //Serial.print(in_byte);
-          sprintf(&response[spot],"%c",in_byte);
-          spot += 1; 
-        }while((Serial.available() > 0) && (in_byte != '\0') && (in_byte != '*'));
-        digitalWrite(B_LED_PIN, FALSE);
-        //Serial.println(response);
-        if (in_char == '$')
-        {
-          digitalWrite(A_LED_PIN, !digitalRead(A_LED_PIN));
-          message_started = 1;
-          message_completed = 0;
-          in_message = (String)'$';
-        }
-        else if (in_char == '*')
-        {
-          digitalWrite(B_LED_PIN, !digitalRead(B_LED_PIN));
-          message_started = 0;
-          message_completed = 1;
-          in_message += '*';
-          in_message += '\0';
-        }
-        else
-        {
-          in_message += (String)'x';
-        }
-        
+            
       
       
       
       break;
     case PRIORITY_SLOW:
       digitalWrite(C_LED_PIN, !digitalRead(C_LED_PIN));
-      //Serial2.print("$NAV,123,");
-      //Serial2.print(temp++);
-      //Serial2.println("*");
-      if ( temp > 256) { temp = 0;}
       break;
     default:
       break;
   }
-  */
+  
 }
 void serialEvent2() {
-  //digitalWrite(A_LED_PIN, LED_OFF);
-  
   while ((Serial2.available()>0) && (in_message_completed == 0)) {
-    digitalWrite(B_LED_PIN, LED_ON);
-      // get the new byte:
       char inChar = (char)Serial2.read(); 
-      //Serial.print(inChar);
-      // add it to the inputString:
       if (inChar == '$')
       {
          in_message_started = 1;
@@ -176,11 +146,25 @@ void serialEvent2() {
       {
         in_message += inChar;
       }
-      // if the incoming character is a newline, set a flag
-      // so the main loop can do something about it:
       if (inChar == '*') {
         in_message_completed = 1;
         in_message_started = 0;
       }
   }
+}
+
+String getValue(String data, char separator, int index)
+{
+ int found = 0;
+  int strIndex[] = {
+0, -1  };
+  int maxIndex = data.length()-1;
+  for(int i=0; i<=maxIndex && found<=index; i++){
+  if(data.charAt(i)==separator || i==maxIndex){
+  found++;
+  strIndex[0] = strIndex[1]+1;
+  strIndex[1] = (i == maxIndex) ? i+1 : i;
+  }
+ }
+  return found>index ? data.substring(strIndex[0], strIndex[1]) : "";
 }
